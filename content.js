a6c3c04ae0e1caa3e645c34cea3edbd0fddcbba6
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2019-10-10 16:26:33 +0300

    added translate button and resource text

--- content.js
+++ content.js
@@ -1,8 +1,5 @@
 document.onselectionchange = function () {
     let content = window.getSelection().toString();
-    
-	chrome.storage.sync.set({ 'content': content },function(){
-        
-	});
+	chrome.storage.sync.set({ 'content': content });
 
 };
--- popup.js
+++ popup.js
@@ -1,30 +1,78 @@
-const API_KEY='<YANDEX TRANSLATE API_KEY>';
+const API_KEY = '<YANDEX TRANSLATE API_KEY>';
+let btnTranslate;
+let responseBox;
+let resourceBox;
+let loader;
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
-
+	
 	try {
-		let code = document.getElementById('code');
+
+		btnTranslate = document.getElementById('btn-translate');
+		responseBox = document.getElementById('response');
+		resourceBox = document.getElementById('resource-text');
+
+		loader = document.getElementById('loader');
+
+		btnTranslate.addEventListener('click', (e) => {
+
+			let text = getResource();
+			if (text) {
+				responseBox.style.display = 'none'
+				loader.style.display = 'block';
+				translate(text).then((val) => {
+					responseBox.innerText = (val);
+					loader.style.display = 'none';
+					responseBox.style.display = 'block'
+
+				})
+			}
+
+		});
+
+
 		chrome.storage.sync.get(['content'], function (stored) {
-            
-			if(stored.content){
-                
 
-                translate(stored.content).then(function(val){
-                    code.innerText=(val);
-                });
-
-            }
-			
+			if (stored.content) {
+				responseBox.style.display = 'none'
+				loader.style.display = 'block';
+				translate(stored.content).then(function (val) {
+					responseBox.innerText = (val);
+					loader.style.display = 'none';
+					responseBox.style.display = 'block'
+				});
+			}
 		})
 
 	} catch (ex) {
-		alert(ex);
+		console.log(ex);
 	}
+
+
+
+
+
 
 }, false);
 
 
+function mockTranslate(text) {
+
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("merhaba dünya")
+		}, 1000);
+	})
+}
+
+
 async function translate(text) {
-	let val= await fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${API_KEY}&text=${text}&lang=en-tr`).then(resp => resp.json());
-	 return (val.text.join());
+	let val = await fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${API_KEY}&text=${text}&lang=en-tr`).then(resp => resp.json());
+	return (val.text.join());
+}
+
+function getResource() {
+	return resourceBox.value;
 }
